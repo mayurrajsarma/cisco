64c84e7b2a15245e0ecc46a38fa51c608a0e13c7
mayurrajsarma <mayurrajsarma202@gmail.com>
2025-03-25 19:11:26 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header1 from './Header1'
+
+const Header = () => {
+  return (
+    <div>
+        <Header1/>
+        <div className='justify-between flex'>
+            <ul className='flex'>
+                <li className='mx-3'>Products and services</li>
+                <li className='mx-3'>Solutions</li>
+                <li className='mx-3'>Support</li>
+                <li className='mx-3'>Learn</li>
+            </ul>
+            <ul className='flex'>
+                <li className='mx-3'>Explore cisco</li>
+                <li className='mx-3'>search</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Header1.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header1 = () => {
+  return (
+    <div>
+        <div className='flex justify-end'>
+            <ul className='flex justify-evenly'>
+                <li className='mx-3'>How to buy</li>
+                <li className='mx-3'>Partners</li>
+                <li className='mx-3'>Log in</li>
+                <li className='mx-3'>EN IN</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header1
