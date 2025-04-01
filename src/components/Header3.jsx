82ba8bb0be152677aeaa133b2b1a04bedfce1401
mayurrajsarma <mayurrajsarma202@gmail.com>
2025-04-02 03:32:24 +0530

    new header

---
+++ src/components/Header3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header3 = () => {
+  return (
+    <div className='bg-gray-100 font-semibold py-4 text-sm flex justify-center'>
+        <p>Cisco and NVIDIA. Bringing secure AI to the Enterprise.</p>
+        <p className='ml-4 text-blue-500'>Learn more &gt;</p>
+    </div>
+  )
+}
+
+export default Header3 ;
